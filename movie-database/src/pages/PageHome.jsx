768684823cfa683b0740b855c991abecdbc05e3a
cat <macy.cordner@gmail.com>
2023-09-05 20:58:26 -0700

    added filters

--- movie-database/src/pages/PageHome.jsx
+++ movie-database/src/pages/PageHome.jsx
@@ -16,6 +16,8 @@ const PageHome = () => {
   const [movieList, setMovieList] = useState([]);
 
     const fetchMovie = async (filter) => {
+      const apiUrl = `${endPointThemes}${filter}?api_key=${apiKey}`;
+      console.log("Fetching data from URL:", apiUrl); // Log the URL
       const options = {
         method: "GET",
         headers: {
@@ -24,19 +26,22 @@ const PageHome = () => {
             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDZkZTJkYmVmZjc0MzVkYWIxMzE3NDFlNmFhYTRlZCIsInN1YiI6IjY0ZWUxODhhNGNiZTEyMDEzODlkNWM2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wP7biHdlHFHu3vEQP1oq3lEjZYVWDt9pWBVv1-YYihU",
         },
       };
-      const res = await fetch(`${endPointThemes}${filter}?api_key=${apiKey}`);
+  
+   
+      const res = await fetch(`${endPointThemes}${filter}?api_key=${apiKey}`, options);
       let data = await res.json();
-      console.log({ data });
+  
       const shortList = data.results.slice(0, 12);
-      console.log(shortList);
+ 
       setMovieList(shortList);
     };
 
-    fetchMovie('now_playing');
+
 
   
   useEffect(() => {
     document.title = `${appTitle} - Home`;
+    fetchMovie('now_playing'); 
   }, []);
 
   const filterMovies = (filter) => {
@@ -52,7 +57,7 @@ const PageHome = () => {
         {categories.map((category, index) => (
           <li key={index} className={categories[index]}
             onClick={() => {
-              filterMovies(categories[index]);
+              filterMovies(category);
             }
             }
           >{categories[index]
