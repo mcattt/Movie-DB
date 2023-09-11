189fcfadb36d45ac92a934465d4af244012c695e
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-09-10 19:00:57 -0700

    added grid to About Page after 900px screen wide

--- movie-database/src/pages/PageAbout.jsx
+++ movie-database/src/pages/PageAbout.jsx
@@ -12,9 +12,9 @@ const PageAbout = () => {
   }, []);
 
   return (
-    <section className="">
+    <section className="tablet-b:grid tablet-b:grid-cols-2">
       <img src={aboutImage} alt="A photo of 3 people watching movies in the theater" className="h-80 w-full object-cover"/>
-      <div className="mt-8">
+      <section className="mt-8 px-4">
         <h2 className="font-bold text-2xl my-8">About <span className="text-bright-orange">Clip</span>Movies</h2>
         <p>
         The ClipMovies movie database is a website where users can find popular, top rated, now playing and upcoming movie listings. Browse for your favourite genres or movies, check out their ratings and see how they match up!
@@ -26,8 +26,8 @@ const PageAbout = () => {
           <p className="px-4 text-center font-bold border-2 border-solid border-bright-orange bg-bright-orange text-dark-purple mb-8 mx-auto sm:bg-transparent sm:text-light-purple sm:border-light-purple rounded-md w-fit sm:hover:border-bright-orange sm:hover:bg-bright-orange sm:hover:text-dark-purple">Clip Movies <br/> Now</p>
       </Link>
       <img src={tmdbLogo} alt="TMDB logo" className="w-32 pb-8"/>
-      <p className="mb-20 pb-12 ">This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
-      </div>
+      <p className="mb-20 pb-12 sm:pb-0 sm:mb-0 ">This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+      </section>
     </section>
   );
 };
