879b6b59a4bbccb131f550fe98ee3b2d1193c1ab
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-09-10 18:42:39 -0700

    added About page mobile version

--- movie-database/src/pages/PageAbout.jsx
+++ movie-database/src/pages/PageAbout.jsx
@@ -2,6 +2,9 @@
 
 import { useEffect } from "react";
 import { appTitle } from "../globals/globals";
+import { Link } from "react-router-dom";
+import aboutImage from "/assets/images/about-image.jpg";
+import tmdbLogo from "/assets/images/tmdb-logo.png";
 
 const PageAbout = () => {
   useEffect(() => {
@@ -9,14 +12,22 @@ const PageAbout = () => {
   }, []);
 
   return (
-    <section>
-      <h2 className="bold text-blue-500">About Page</h2>
-      <p>
-        Saepe vitae deserunt cupiditate vel reiciendis adipisci quasi. At,
-        dolore qui, saepe similique id repellat ipsam sapiente repellendus
-        commodi deleniti natus itaque hic temporibus nam nobis tempora enim
-        suscipit quas!
-      </p>
+    <section className="">
+      <img src={aboutImage} alt="A photo of 3 people watching movies in the theater" className="h-80 w-full object-cover"/>
+      <div className="mt-8">
+        <h2 className="font-bold text-2xl my-8">About <span className="text-bright-orange">Clip</span>Movies</h2>
+        <p>
+        The ClipMovies movie database is a website where users can find popular, top rated, now playing and upcoming movie listings. Browse for your favourite genres or movies, check out their ratings and see how they match up!
+      </p> <br />
+      <p className="mb-8">
+        Found something you like? Add them to your favourites and remove any item anytime.
+        </p>
+        <Link to="/">
+          <p className="px-4 text-center font-bold border-2 border-solid border-bright-orange bg-bright-orange text-dark-purple mb-8 mx-auto sm:bg-transparent sm:text-light-purple sm:border-light-purple rounded-md w-fit sm:hover:border-bright-orange sm:hover:bg-bright-orange sm:hover:text-dark-purple">Clip Movies <br/> Now</p>
+      </Link>
+      <img src={tmdbLogo} alt="TMDB logo" className="w-32 pb-8"/>
+      <p className="mb-20 pb-12 ">This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+      </div>
     </section>
   );
 };
