4469a89e45f8a35e0f8269fb67390d5d3da4558b
cat <macy.cordner@gmail.com>
2023-09-24 11:48:06 -0700

    changed favorites icon when its empty

--- movie-database/src/pages/PageFavourites.jsx
+++ movie-database/src/pages/PageFavourites.jsx
@@ -14,15 +14,16 @@ const PageFavourites = () => {
   return (
     <section>
       {favs.length < 1 ? (
-        <div className="flex  flex-col items-center  justify-center mt-5 breakpoint-med:mt-36 ">
-          <img src={sadFace} alt="" className=" " />
-          <p className=" text-center pt-10 text-3xl px-3">
+        <div className="flex  flex-col items-center  justify-center mt-10  ">
+          <img src={sadFace} alt="" className=" w-[14rem]" />
+          <p className=" text-center pt-10 text-2xl px-3">
             Sorry, you have no favourite movies. Return to the home page to add
             your favourite movies.
           </p>
           <Link to={`/`}>
-            <button className="w-60 rounded-xl mb-[88px] mt-24 breakpoint-med:mt-32 group/button outline-light-purple outline outline-1  active:outline-none breakpoint-med:hover:outline-none active:bg-orange-500 breakpoint-med:hover:bg-orange-500 transition-all ">
-              <p className="m-1 text-light-purple font-bold text-3xl group-active/button:text-dark-purple breakpoint-med:group-hover/button:text-dark-purple">
+            <button className="  w-48 mt-10 rounded-xl group/button outline-light-purple outline outline-1  active:outline-none breakpoint-med:hover:outline-none active:bg-orange-500 breakpoint-med:hover:bg-orange-500 transition-all ">
+              <p className="px-[21px] leading-tight text-light-purple font-bold text-2xl group-active/button:text-dark-purple breakpoint-med:group-hover/button:text-dark-purple">
+                
                 Clip Movies Now
               </p>
             </button>
