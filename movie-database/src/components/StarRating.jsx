89e3736bfeadee57eeeae9c45459d99b1529721a
sasuke631 <39613236+sasuke631@users.noreply.github.com>
2023-10-07 23:19:23 -0700

    made notes on StarRating

--- movie-database/src/components/StarRating.jsx
+++ movie-database/src/components/StarRating.jsx
@@ -5,19 +5,21 @@ import { StarOutlined } from "@ant-design/icons";
 function StarRating({ vote_average }) {
   const [isZero, setIsZero] = useState(false);
 
-  if (
-    vote_average === null ||
-    vote_average === undefined ||
-    vote_average < 0.5
-  ) {
-    setIsZero(true);
-  }
+  // THIS IF CONDITION BREAKS THE SEARCH FUINCTION
+  // if (
+  //   vote_average === null ||
+  //   vote_average === undefined ||
+  //   vote_average < 0.5
+  // ) {
+  //   setIsZero(true);
+  // }
 
   return (
     <>
       {isZero ? (
         <Rate defaultValue={5} disabled character={<StarOutlined />} />
       ) : (
+        // Something is off with the visual star representation vs vote number
         <Rate defaultValue={vote_average / 2} allowHalf disabled />
       )}
     </>
