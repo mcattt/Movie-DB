b432c40b8f3883fc8f541a675e984e982a604e92
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-10 00:12:20 -0700

    added active state using useLocation on ThumbNav.jsx

--- movie-database/src/components/ThumbNav.jsx
+++ movie-database/src/components/ThumbNav.jsx
@@ -2,10 +2,17 @@ import { FaInfoCircle } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
 import { FaRegHeart } from "react-icons/fa";
 import { IconContext } from "react-icons";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import React from "react";
 
 const ThumbNav = () => {
+
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <IconContext.Provider
       value={{ color: "#CA98E5", size: "2rem", className: "global-class-name" }}
@@ -14,24 +21,24 @@ const ThumbNav = () => {
         <ul className="text-light-purple text-center uppercase grid grid-cols-3">
           <li>
             <NavLink to="/">
-              <div className="flex flex-col items-center ">
-                <IoHome /> 
+              <div className={`flex flex-col items-center ${isActive("/") ? "text-bright-orange" : ""}`}>
+                <IoHome className={`${isActive("/") ? "fill-orange-500" : "fill-light-purple"}`}/> 
                 <span className="pt-1 text-sm font-bold">Home</span>
               </div>
             </NavLink>
           </li>
           <li>
             <NavLink to="/about">
-              <div className="flex flex-col items-center ">
-                <FaInfoCircle /> 
+              <div className={`flex flex-col items-center ${isActive("/about") ? "text-bright-orange" : ""}`}>
+                <FaInfoCircle className={`${isActive("/about") ? "fill-orange-500" : "fill-light-purple"}`}/> 
                 <span className="pt-1 text-sm font-bold">About</span>
               </div>
             </NavLink>
           </li>
           <li>
             <NavLink to="/favourites">
-              <div className="flex flex-col items-center ">
-                <FaRegHeart /> 
+              <div className={`flex flex-col items-center ${isActive("/favourites") ? "text-bright-orange" : ""}`}>
+                <FaRegHeart className={`${isActive("/favourites") ? "fill-orange-500" : "fill-light-purple"}`}/> 
                 <span  className="pt-1 text-sm font-bold">Favourites</span>
               </div>
             </NavLink>
