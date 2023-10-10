94e637c7ff7c3b582ae9c67e0e81250620fcc9dc
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-10 01:03:12 -0700

    fixed the error related to exact attribute on NavLink - Nav.jsx

--- movie-database/src/components/Nav.jsx
+++ movie-database/src/components/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
     <nav className="main-nav self-center hidden sm:block " onClick={blur}>
       <ul className="text-light-purple font-bold flex sm:text-2xl lg:text-3xl xl:text-4xl">
         <li className="px-2 mr-8 border-transparent border-2 border-solid rounded-lg hover:text-bright-orange hover:border-solid hover:border-2 hover:border-bright-orange hover:rounded-lg tablet-s:mr-16 tablet-b:mr-32">
-          <NavLink exact to="/"
+          <NavLink to="/"
             className={location.pathname === "/" ? "text-bright-orange" : ""}>Home</NavLink>
         </li>
         <li className="px-2 mr-8 border-transparent border-2 border-solid rounded-lg hover:text-bright-orange hover:border-solid hover:border-2 hover:border-bright-orange hover:rounded-lg tablet-s:mr-16 tablet-b:mr-32">
