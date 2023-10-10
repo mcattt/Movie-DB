b05750cbfb3446909c3f944eef96325b04e30dcb
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-10 16:35:11 -0700

    added delayed Loading component on Single Movie Page

--- movie-database/src/components/Loading.jsx
+++ movie-database/src/components/Loading.jsx
@@ -1,9 +1,11 @@
-import loading from '/assets/gifs/loading.gif';
+import loading from '/assets/gifs/clip-loading.gif';
 
 const Loading = () => {
     
     return (
-        <img src={loading} alt="Loading" className="block my-0 mx-auto" id="loading" />
+        <div className='absolute top-[40%] left-0 right-0'>
+            <img src={loading} alt="Loading" className="block my-0 mx-auto w-[48px]" id="loading" />
+        </div>
     )
 
 }
