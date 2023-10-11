8b7fc336040ffc174a59a925c946d5504c52af80
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-10 21:04:23 -0700

    adjusted margin left to Logo Header

--- movie-database/src/components/Header.jsx
+++ movie-database/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className="header sm:flex sm:justify-between sm:content-center py-4 bg-dark-purple shadow-[0_3px_1px_0px_rgba(134,134,134,0.16)]">
       <Link to="/">
         <img
-          className="xl:w-40 w-32 sm:ml-16 mx-auto sm:my-0 "
+          className="xl:w-40 w-32 sm:ml-8 mx-auto sm:my-0 "
           src={headerLogo}
           alt="Logo"
         />
