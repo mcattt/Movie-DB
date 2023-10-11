fc12676ebdf20d99f8bd2e394b5e108617146238
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-10 17:46:44 -0700

    fixed padding for the Nav Menu on table-s and table-b screen

--- movie-database/src/components/Nav.jsx
+++ movie-database/src/components/Nav.jsx
@@ -11,11 +11,11 @@ const Nav = () => {
   return (
     <nav className="main-nav self-center hidden sm:block " onClick={blur}>
       <ul className="text-light-purple font-bold flex sm:text-2xl lg:text-3xl xl:text-4xl">
-        <li className="px-2 mr-8 border-transparent border-2 border-solid rounded-lg hover:text-bright-orange hover:border-solid hover:border-2 hover:border-bright-orange hover:rounded-lg tablet-s:mr-16 tablet-b:mr-32 transition duration-500">
+        <li className="px-2 mr-8 border-transparent border-2 border-solid rounded-lg hover:text-bright-orange hover:border-solid hover:border-2 hover:border-bright-orange hover:rounded-lg tablet-s:mr-8 tablet-b:mr-16 transition duration-500">
           <NavLink to="/"
             className={location.pathname === "/" ? "text-bright-orange" : ""}>Home</NavLink>
         </li>
-        <li className="px-2 mr-8 border-transparent border-2 border-solid rounded-lg hover:text-bright-orange hover:border-solid hover:border-2 hover:border-bright-orange hover:rounded-lg tablet-s:mr-16 tablet-b:mr-32 transition duration-500">
+        <li className="px-2 mr-8 border-transparent border-2 border-solid rounded-lg hover:text-bright-orange hover:border-solid hover:border-2 hover:border-bright-orange hover:rounded-lg tablet-s:mr-8 tablet-b:mr-16 transition duration-500">
           <NavLink to="/about"
             className={location.pathname === "/about" ? "text-bright-orange" : ""}>About</NavLink>
         </li>
