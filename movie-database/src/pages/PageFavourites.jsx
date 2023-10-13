728bda96a73ed68c2ebe472dcdd149a2f05d12b0
cat <macy.cordner@gmail.com>
2023-10-13 12:55:42 -0700

    fixed margin bottom on fave page

--- movie-database/src/pages/PageFavourites.jsx
+++ movie-database/src/pages/PageFavourites.jsx
@@ -18,18 +18,28 @@ const PageFavourites = () => {
     <section data-aos="fade-in" className="mb-16 sm:mb-0">
       {favs.length < 1 ? (
         <div className="flex  flex-col items-center  justify-center mt-10 ">
-          <img data-aos="fade-in"
+          <img
+            data-aos="fade-in"
             src={sadFace}
             alt=""
             className=" w-[14rem] breakpoint-small:mt-24"
           />
-          <p data-aos="fade-in" className=" text-center pt-10 text-2xl breakpoint-small:text-3xl px-3">
+          <p
+            data-aos="fade-in"
+            className=" text-center pt-10 text-2xl breakpoint-small:text-3xl px-3"
+          >
             Sorry, you have no favourite movies. Return to the home page to add
             your favourite movies.
           </p>
           <Link to={`/`}>
-            <button data-aos="fade-in" className="border-2 border-solid rounded-md w-48 mt-10 mb-[88px] bg-bright-orange border-bright-orange group/button tablet-b:bg-transparent tablet-b:border-light-purple  tablet-b:active:border-orange-500 tablet-b:hover:border-orange-500 active:bg-orange-500 tablet-b:hover:bg-orange-500 transition duration-500 ">
-              <p data-aos="fade-in" className="px-4 text-center leading-tight text-dark-purple tablet-b:text-light-purple font-bold text-2xl  tablet-b:group-active/button:text-dark-purple tablet-b:group-hover/button:text-dark-purple">
+            <button
+              data-aos="fade-in"
+              className="border-2 border-solid rounded-md w-48 mt-10 mb-[88px] bg-bright-orange border-bright-orange group/button tablet-b:bg-transparent tablet-b:border-light-purple  tablet-b:active:border-orange-500 tablet-b:hover:border-orange-500 active:bg-orange-500 tablet-b:hover:bg-orange-500 transition duration-500 "
+            >
+              <p
+                data-aos="fade-in"
+                className="px-4 text-center leading-tight text-dark-purple tablet-b:text-light-purple font-bold text-2xl  tablet-b:group-active/button:text-dark-purple tablet-b:group-hover/button:text-dark-purple"
+              >
                 Clip Movies Now
               </p>
             </button>
@@ -40,7 +50,7 @@ const PageFavourites = () => {
           <h2 className="text-5xl font-bold mt-16 ml-12 mb-5">
             Your Favourites
           </h2>
-          <div className="grid grid-cols-1 breakpoint-small:grid-cols-2 breakpoint-med:grid-cols-3 breakpoint-large:grid-cols-4 justify-items-center mb-[88px] ">
+          <div className="grid grid-cols-1 breakpoint-small:grid-cols-2 breakpoint-med:grid-cols-3 breakpoint-large:grid-cols-4 justify-items-center mb-[50px] tablet-b:mb-0 ">
             {favs.map((singleMovie, i) => (
               <MovieCard key={i} movie={singleMovie} isFav={true} />
             ))}
